a854f9bd300e5209c4612627b12652352913a390
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-09-25 19:02:57 -0500

    Segundo Cambio

--- 01-Estructuras/recordando.py
+++ 01-Estructuras/recordando.py
@@ -1,1 +1,13 @@
-nombres = ["daniel", "otro"]
+names = ["daniel", "otro", "Fernanda"]
+numbers = [1,2,3,4,5,6,1,1,1,7,8,9]
+
+names.insert(3, "Arturo")
+index_name = names.index("Fernanda")
+number_one = numbers.count(1)
+numbers.sort()
+numbers.sort(reverse=True)
+
+print(names)
+print(index_name)
+print(number_one)
+print(numbers)
