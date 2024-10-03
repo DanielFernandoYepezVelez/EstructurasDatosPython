d172310a210ed13bf525c800fd2af3586b3ce28a
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-10-02 20:40:55 -0500

    Desarrollando Ejercicios En Clase

--- 01-Estructuras/03-Diccionarios/01-ejemplo_clase.py
+++ 01-Estructuras/03-Diccionarios/01-ejemplo_clase.py
@@ -15,3 +15,28 @@ personal_information_two = dict(
 )
 
 print(type(personal_information_two))
+
+# Acceder personal information
+# print(personal_information_one.get('name'))
+print(personal_information_one['name'])
+print(personal_information_one['age'])
+personal_information_one['age'] = 45
+personal_information_one['birth_date'] = '07/01/28'
+
+print(personal_information_one['birth_date'])
+
+# pop()
+# popitem()
+# del, clear
+# keys(), values(), items(),
+print('\n===== KEYS =====')
+for key in personal_information_one.keys():
+    print(key)
+
+print('\n======= VALUES =======')
+for key in personal_information_one.values():
+    print(key)
+
+print('\n======= ITEMS =======')
+for key in personal_information_one.items():
+    print(key)
---
+++ 01-Estructuras/03-Diccionarios/01-ejercicio_clase.py
@@ -0,0 +1,48 @@
+"""
+    Solicitar Un Número Por Teclado
+    Y crear un diccionario, cuyas claves
+    sean desde 1 hasta el numero ingresado
+    y los valores sean el cubo de las claves.
+    Mostrar claves y su valor correspondiente. 
+"""
+"""
+
+# Solicitar un número al usuario
+numero = int(input("Ingresa un número: "))
+
+# Crear un diccionario con claves desde 1 hasta el número ingresado
+diccionario = {
+    i: i ** 3 for i in range(1, numero + 1)
+}
+
+# Mostrar claves y sus valores correspondientes
+for clave, valor in diccionario.items():
+    print(f"{clave}: {valor}")
+
+"""
+
+# Definir E Inicializar Un Diccionario
+fruits = {
+    'manzana': 2000,
+    'pera': 1500,
+    'naranja': 1800,
+    'uva': 3000,
+    'kiwi': 2500,
+    'mango': 2200,
+}
+
+# Solicitar El Nombre De Una Fruta Por Consola
+name_fruit = input('Ingrese El Nombre De Una Fruta: ')
+
+# Validar Si La Fruta Existe En El Diccionario
+if name_fruit in fruits.keys():
+    # Solicitar La Cantidad De La Fruta Por Consola
+    quantity = int(input('Ingrese La Cantidad Que Desea Comprar: '))
+
+    # Procesos Aritméticos
+    price = quantity * fruits[name_fruit]
+    
+    # Mostrar Información Final Por Consola
+    print(f'El Precio Total De La Compra Es: {price}')
+else:
+    print('La Fruta Ingresada No Existe En El Sistema.')
